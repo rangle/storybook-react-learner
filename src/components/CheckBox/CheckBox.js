e65bd07c6d717877d0a61b6e51d91627ada02318
roofus <roofus@roofuss-mbp.lan>
2022-02-16 09:36:13 -0500

    changed css to test chromatic changes

--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -20,7 +20,7 @@ const Input = styled.input.attrs({ type: 'checkbox' })`
   ${props =>
     props.isSelected && !props.isInvalid &&
     `
-      background-color: black;
+      background-color: blue;
       border: 2px solid black;
 
       &:checked::before {
